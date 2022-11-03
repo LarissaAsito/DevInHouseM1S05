79dd6d182a16adee2f295d34172c608408027f8f
Larissa <larissa.asito@gmail.com>
2022-11-03 00:03:25 -0300

    Adicionando o exercicio 2

---
+++ ex2/Fatura.js
@@ -0,0 +1,25 @@
+export default class Fatura {
+    id
+    descricao
+    quantia
+    preco
+  
+    constructor(id, descricao, quantia, preco) {
+        this.id = id;
+        this.descricao = descricao;
+        if (quantia < 0 ) {
+            this.quantia = 0;
+        } else {
+            this.quantia = quantia;
+        }
+        if (preco < 0 ) {
+            this.preco = 0;
+        } else {
+            this.preco = preco;
+        }
+    }
+  
+    obterValorTotal(percentual) {
+        return this.quantia * this.preco;
+    }
+}
